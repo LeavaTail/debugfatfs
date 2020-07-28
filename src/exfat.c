51b3534086f24bbbfe9ba4abc38c8a49cc7aecfa
LeavaTail <starbow.duster@gmail.com>
2020-07-28 22:55:28 +0900

    [Improve] Reorder function definition

--- src/exfat.c
+++ src/exfat.c
@@ -5,6 +5,62 @@
 #include <time.h>
 #include "dumpexfat.h"
 
+/* Print function prototype */
+int exfat_show_boot_sec(struct device_info *, struct exfat_bootsec *);
+static void exfat_print_allocation_bitmap(struct device_info *);
+static void exfat_print_upcase_table(struct device_info *);
+static void exfat_print_file_entry(struct device_info *, union dentry, union dentry, union dentry, uint8_t);
+int exfat_print_cluster(struct device_info *, uint32_t);
+/* Load function prototype */
+static int exfat_create_allocation_chain(struct device_info *, void *);
+static void exfat_load_filename(union dentry, uint64_t, uint8_t);
+static void exfat_load_timestamp(struct tm *, char *, uint32_t, uint8_t, uint8_t);
+int exfat_load_root_dentry(struct device_info *, void *);
+int exfat_traverse_directory(struct device_info *, uint32_t);
+static int __exfat_traverse_directory(struct device_info *, uint32_t, size_t);
+/* Check function prototype */
+static bool exfat_check_allocation_cluster(struct device_info *, uint32_t);
+
+
+/**
+ * exfat_show_boot_sec - show boot sector in exFAT
+ * @info:       Target device information
+ * @b:          boot sector pointer in exFAT
+ *
+ * TODO: Unify the words (show? print? display?)
+ */
+int exfat_show_boot_sec(struct device_info *info, struct exfat_bootsec *b)
+{
+	dump_info("%-28s\t: %8lx (sector)\n", "media-relative sector offset", b->PartitionOffset);
+	dump_info("%-28s\t: %8x (sector)\n", "Offset of the First FAT", b->FatOffset);
+	dump_info("%-28s\t: %8u (sector)\n", "Length of FAT table", b->FatLength);
+	dump_info("%-28s\t: %8x (sector)\n", "Offset of the Cluster Heap", b->ClusterHeapOffset);
+	dump_info("%-28s\t: %8u (cluster)\n", "The number of clusters", b->ClusterCount);
+	dump_info("%-28s\t: %8u (cluster)\n", "The first cluster of the root", b->FirstClusterOfRootDirectory);
+
+	info->fat_offset = b->FatOffset;
+	info->heap_offset = b->ClusterHeapOffset;
+	info->root_offset = b->FirstClusterOfRootDirectory;
+	info->sector_size  = 1 << b->BytesPerSectorShift;
+	info->cluster_shift = b->SectorsPerClusterShift;
+	info->cluster_count = b->ClusterCount;
+	info->fat_length = b->NumberOfFats * b->FatLength * info->sector_size;
+
+	dump_notice("%-28s\t: %8lu (sector)\n", "Size of exFAT volumes", b->VolumeLength);
+	dump_notice("%-28s\t: %8lu (byte)\n", "Bytes per sector", info->sector_size);
+	dump_notice("%-28s\t: %8lu (byte)\n", "Bytes per cluster", info->sector_size * (1 << info->cluster_shift));
+
+	dump_notice("%-28s\t: %8u\n", "The number of FATs", b->NumberOfFats);
+	dump_notice("%-28s\t: %8u (%%)\n", "The percentage of clusters", b->PercentInUse);
+	dump_notice("\n");
+
+	return 0;
+}
+
+/**
+ * exfat_print_allocation_bitmap - print allocation bitmap
+ * @info:       Target device information
+ */
 static void exfat_print_allocation_bitmap(struct device_info *info)
 {
 	node_t *node = info->chain_head;
@@ -15,12 +71,17 @@ static void exfat_print_allocation_bitmap(struct device_info *info)
 	dump_notice("\n");
 }
 
+/**
+ * exfat_print_upcase-table - print upcase table
+ * @info:       Target device information
+ */
 static void exfat_print_upcase_table(struct device_info *info)
 {
 	int byte, offset;
 	size_t uni_count = 0x10 / sizeof(uint16_t);
 	size_t length = info->upcase_size;
 
+	/* Usually, we don't want to display raw upcase table */
 	if (print_level < DUMP_INFO) {
 		 dump_notice("Upcase-table was skipped.\n");
 		 return;
@@ -42,31 +103,13 @@ static void exfat_print_upcase_table(struct device_info *info)
 	}
 }
 
-static void exfat_load_timestamp(struct tm *t, char *str, uint32_t time, uint8_t subsec, uint8_t tz)
-{
-	int8_t offset = tz * 15;
-	t->tm_year = (time >> EXFAT_YEAR) & 0x7f;
-	t->tm_mon  = (time >> EXFAT_MONTH) & 0x0f;
-	t->tm_mday = (time >> EXFAT_DAY) & 0x1f;
-	t->tm_hour = (time >> EXFAT_HOUR) & 0x0f;
-	t->tm_min  = (time >> EXFAT_MINUTE) & 0x3f;
-	t->tm_sec  = (time & 0x1f) * 2;
-	t->tm_sec += subsec / 100;
-	dump_info("%s: %d-%02d-%02d %02d:%02d:%02d.%02d (UTC + %02d:%02d)\n", str,
-			1980 + t->tm_year, t->tm_mon, t->tm_mday,
-			t->tm_hour, t->tm_min,
-			t->tm_sec,
-			subsec % 100,
-			offset / 60,
-			offset % 60);
-}
-
-static void exfat_load_filename(union dentry name, uint64_t name_len, uint8_t count)
-{
-	/* FIXME: decode and output name */
-	dump_info("Name: Optimized out. (%lu)\n", name_len);
-}
-
+/**
+ * exfat_print_file_entry - print file metadata
+ * @info:       Target device information
+ * @file:       file dentry
+ * @stream:     stream Extension dentry
+ * @name:       File Name dentry
+ */
 static void exfat_print_file_entry(struct device_info *info, union dentry file, union dentry stream, union dentry name, uint8_t count)
 {
 	struct tm ctime, mtime, atime;
@@ -81,6 +124,35 @@ static void exfat_print_file_entry(struct device_info *info, union dentry file, 
 	dump_info("\n");
 }
 
+/**
+ * exfat_show_boot_sec - function to print any cluster
+ * @info:       Target device information
+ * @index:      cluster index to display
+ *
+ * TODO: Guess the entry in cluster
+ */
+int exfat_print_cluster(struct device_info *info, uint32_t index)
+{
+	if (!exfat_check_allocation_cluster(info, index) && !info->force) {
+		dump_err("cluster %u is not allocated.\n", index);
+		return -1;
+	}
+
+	void *data = get_cluster(info, index);
+	if (!data)
+		return -1;
+
+	dump_notice("Cluster #%u:\n", index);
+	size_t size = ((1 << info->cluster_shift) * info->sector_size);
+	hexdump(output, data, size);
+	return 0;
+}
+
+/**
+ * exfat_create_allocation_chain - function to get cluster chain
+ * @info:       Target device information
+ * @void:       pointer to Bitmap table
+ */
 static int exfat_create_allocation_chain(struct device_info *info, void *bitmap)
 {
 	int i, bit;
@@ -100,14 +172,118 @@ static int exfat_create_allocation_chain(struct device_info *info, void *bitmap)
 	return 0;
 }
 
-static bool exfat_check_allocation_cluster(struct device_info *info, uint32_t index)
+/**
+ * exfat_load_filename - function to get filename
+ * @name:           filename dentry
+ * @name_len:       filename length
+ * @count:          the number of filename dentries
+ */
+static void exfat_load_filename(union dentry name, uint64_t name_len, uint8_t count)
 {
-	node_t *node = search_node(info->chain_head, index);
-	if (node)
-		return true;
-	return false;
+	/* FIXME: decode and output name */
+	dump_info("Name: Optimized out. (%lu)\n", name_len);
 }
 
+/**
+ * exfat_load_timestamp - function to get timestamp in file
+ * @tm:             output pointer
+ * @str             additional any messages
+ * @time:           Timestamp Field in File Directory Entry
+ * @subsec:         10msincrement Field in File Directory Entry
+ * @tz:             UtcOffset in File Directory Entry
+ */
+static void exfat_load_timestamp(struct tm *t, char *str, uint32_t time, uint8_t subsec, uint8_t tz)
+{
+	int8_t offset = tz * 15;
+	t->tm_year = (time >> EXFAT_YEAR) & 0x7f;
+	t->tm_mon  = (time >> EXFAT_MONTH) & 0x0f;
+	t->tm_mday = (time >> EXFAT_DAY) & 0x1f;
+	t->tm_hour = (time >> EXFAT_HOUR) & 0x0f;
+	t->tm_min  = (time >> EXFAT_MINUTE) & 0x3f;
+	t->tm_sec  = (time & 0x1f) * 2;
+	t->tm_sec += subsec / 100;
+	dump_info("%s: %d-%02d-%02d %02d:%02d:%02d.%02d (UTC + %02d:%02d)\n", str,
+			1980 + t->tm_year, t->tm_mon, t->tm_mday,
+			t->tm_hour, t->tm_min,
+			t->tm_sec,
+			subsec % 100,
+			offset / 60,
+			offset % 60);
+}
+
+/**
+ * exfat_load_root_dentry - load extra entry(bitmap/upcase/volume)
+ * @info:          Target device information
+ * @root:          root directory dentry
+ */
+int exfat_load_root_dentry(struct device_info *info, void *root)
+{
+	int i, byte;
+	for(i = 0;
+			i < (((1 << info->cluster_shift) * info->sector_size) / sizeof(struct exfat_dentry));
+			i++) {
+		struct exfat_dentry dentry = ((struct exfat_dentry *)root)[i];
+		switch (dentry.EntryType) {
+			case DENTRY_BITMAP:
+				dump_debug("Get: allocation table: cluster %x, size: %lx\n",
+						dentry.dentry.bitmap.FirstCluster,
+						dentry.dentry.bitmap.DataLength);
+				info->chain_head = init_node();
+				void *data = get_cluster(info, dentry.dentry.bitmap.FirstCluster);
+				exfat_create_allocation_chain(info, data);
+				free(data);
+				dump_notice("Allocation Bitmap (#%u):\n", dentry.dentry.bitmap.FirstCluster);
+				exfat_print_allocation_bitmap(info);
+				break;
+			case DENTRY_UPCASE:
+				info->upcase_size = dentry.dentry.upcase.DataLength;
+				size_t clusize = 1 << info->cluster_shift * info->sector_size;
+				size_t clusters = (clusize / info->upcase_size) + 1;
+				info->upcase_table = (uint16_t*)malloc(clusize * clusters);
+				dump_debug("Get: Up-case table: cluster %x, size: %x\n",
+						dentry.dentry.upcase.FirstCluster,
+						dentry.dentry.upcase.DataLength);
+				info->upcase_table = get_clusters(info, dentry.dentry.upcase.FirstCluster, clusters);
+				dump_notice("Allocation Bitmap (#%u):\n", dentry.dentry.upcase.FirstCluster);
+				exfat_print_upcase_table(info);
+				break;
+			case DENTRY_VOLUME:
+				dump_notice("volume Label: ");
+				/* FIXME: VolumeLabel is Unicode string, But %c is ASCII code */
+				for(byte = 0; byte < dentry.dentry.vol.CharacterCount; byte++) {
+					dump_notice("%c", dentry.dentry.vol.VolumeLabel[byte]);
+				}
+				dump_notice("\n");
+				break;
+			case DENTRY_GUID:
+				break;
+			case DENTRY_UNUSED:
+				goto out;
+		}
+	}
+out:
+	return 0;
+}
+
+/**
+ * __exfat_traverse_directory - function interface to traverse all cluster
+ * @info:          Target device information
+ * @index:         index of the cluster want to check
+ */
+int exfat_traverse_directory(struct device_info *info, uint32_t index)
+{
+	info->root = (node2_t **)malloc(sizeof(node2_t *) * info->root_maxsize);
+	return __exfat_traverse_directory(info, index, 0);
+}
+
+/**
+ * __exfat_traverse_directory - function to traverse all cluster
+ * @info:          Target device information
+ * @index:         index of the cluster want to check
+ * @count:         Directory depth count
+ *
+ * TODO: check FAT entry for discontinuous index
+ */
 static int __exfat_traverse_directory(struct device_info *info, uint32_t index, size_t count)
 {
 	int i;
@@ -170,102 +346,16 @@ exfat_print_file_entry(info, d.dentry, next.dentry, d.dentry, d.dentry.file.Seco
 	return 0;
 }
 
-int exfat_traverse_directory(struct device_info *info, uint32_t index)
+/**
+ * exfat_check_allocation_cluster - Whether or not cluster is allocated
+ * @info:          Target device information
+ * @index:         index of the cluster want to check
+ */
+static bool exfat_check_allocation_cluster(struct device_info *info, uint32_t index)
 {
-	info->root = (node2_t **)malloc(sizeof(node2_t *) * info->root_maxsize);
-	return __exfat_traverse_directory(info, index, 0);
+	node_t *node = search_node(info->chain_head, index);
+	if (node)
+		return true;
+	return false;
 }
 
-int exfat_load_root_dentry(struct device_info *info, void *root)
-{
-	int i, byte;
-	for(i = 0;
-			i < (((1 << info->cluster_shift) * info->sector_size) / sizeof(struct exfat_dentry));
-			i++) {
-		struct exfat_dentry dentry = ((struct exfat_dentry *)root)[i];
-		switch (dentry.EntryType) {
-			case DENTRY_BITMAP:
-				dump_debug("Get: allocation table: cluster %x, size: %lx\n",
-						dentry.dentry.bitmap.FirstCluster,
-						dentry.dentry.bitmap.DataLength);
-				info->chain_head = init_node();
-				void *data = get_cluster(info, dentry.dentry.bitmap.FirstCluster);
-				exfat_create_allocation_chain(info, data);
-				free(data);
-				dump_notice("Allocation Bitmap (#%u):\n", dentry.dentry.bitmap.FirstCluster);
-				exfat_print_allocation_bitmap(info);
-				break;
-			case DENTRY_UPCASE:
-				info->upcase_size = dentry.dentry.upcase.DataLength;
-				size_t clusize = 1 << info->cluster_shift * info->sector_size;
-				size_t clusters = (clusize / info->upcase_size) + 1;
-				info->upcase_table = (uint16_t*)malloc(clusize * clusters);
-				dump_debug("Get: Up-case table: cluster %x, size: %x\n",
-						dentry.dentry.upcase.FirstCluster,
-						dentry.dentry.upcase.DataLength);
-				info->upcase_table = get_clusters(info, dentry.dentry.upcase.FirstCluster, clusters);
-				dump_notice("Allocation Bitmap (#%u):\n", dentry.dentry.upcase.FirstCluster);
-				exfat_print_upcase_table(info);
-				break;
-			case DENTRY_VOLUME:
-				dump_notice("volume Label: ");
-				/* FIXME: VolumeLabel is Unicode string, But %c is ASCII code */
-				for(byte = 0; byte < dentry.dentry.vol.CharacterCount; byte++) {
-					dump_notice("%c", dentry.dentry.vol.VolumeLabel[byte]);
-				}
-				dump_notice("\n");
-				break;
-			case DENTRY_GUID:
-				break;
-			case DENTRY_UNUSED:
-				goto out;
-		}
-	}
-out:
-	return 0;
-}
-
-int exfat_show_boot_sec(struct device_info *info, struct exfat_bootsec *b)
-{
-	dump_info("%-28s\t: %8lx (sector)\n", "media-relative sector offset", b->PartitionOffset);
-	dump_info("%-28s\t: %8x (sector)\n", "Offset of the First FAT", b->FatOffset);
-	dump_info("%-28s\t: %8u (sector)\n", "Length of FAT table", b->FatLength);
-	dump_info("%-28s\t: %8x (sector)\n", "Offset of the Cluster Heap", b->ClusterHeapOffset);
-	dump_info("%-28s\t: %8u (cluster)\n", "The number of clusters", b->ClusterCount);
-	dump_info("%-28s\t: %8u (cluster)\n", "The first cluster of the root", b->FirstClusterOfRootDirectory);
-
-	info->fat_offset = b->FatOffset;
-	info->heap_offset = b->ClusterHeapOffset;
-	info->root_offset = b->FirstClusterOfRootDirectory;
-	info->sector_size  = 1 << b->BytesPerSectorShift;
-	info->cluster_shift = b->SectorsPerClusterShift;
-	info->cluster_count = b->ClusterCount;
-	info->fat_length = b->NumberOfFats * b->FatLength * info->sector_size;
-
-	dump_notice("%-28s\t: %8lu (sector)\n", "Size of exFAT volumes", b->VolumeLength);
-	dump_notice("%-28s\t: %8lu (byte)\n", "Bytes per sector", info->sector_size);
-	dump_notice("%-28s\t: %8lu (byte)\n", "Bytes per cluster", info->sector_size * (1 << info->cluster_shift));
-
-	dump_notice("%-28s\t: %8u\n", "The number of FATs", b->NumberOfFats);
-	dump_notice("%-28s\t: %8u (%%)\n", "The percentage of clusters", b->PercentInUse);
-	dump_notice("\n");
-
-	return 0;
-}
-
-int exfat_print_cluster(struct device_info *info, uint32_t index)
-{
-	if (!exfat_check_allocation_cluster(info, index) && !info->force) {
-		dump_err("cluster %u is not allocated.\n", index);
-		return -1;
-	}
-
-	void *data = get_cluster(info, index);
-	if (!data)
-		return -1;
-
-	dump_notice("Cluster #%u:\n", index);
-	size_t size = ((1 << info->cluster_shift) * info->sector_size);
-	hexdump(output, data, size);
-	return 0;
-}
