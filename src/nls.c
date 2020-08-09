fe498c3cf7c7feade7b4f3111baaeaf58606b997
LeavaTail <starbow.duster@gmail.com>
2020-08-09 22:44:26 +0900

    [Improve] Clarified the order of evaluation in utf32_to_utf8

--- src/nls.c
+++ src/nls.c
@@ -59,19 +59,19 @@ int utf32_to_utf8(uint32_t u, unsigned char *d)
 		*d = (unsigned char)u;
 		len = 1;
 	} else if (u < 0x7FF) {
-		*d++ = (0xC0 | u >> 6);
-		*d++ = (0x80 | u & 0x3F);
+		*d++ = (0xC0 | (u >> 6));
+		*d++ = (0x80 | (u & 0x3F));
 		len = 2;
 	} else if (u < 0xFFFF) {
-		*d++ = (0xE0 | u >> 12);
-		*d++ = (0x80 | (u >> 6) & 0x3f);
-		*d++ = (0x80 | u & 0x3F);
+		*d++ = (0xE0 | (u >> 12));
+		*d++ = (0x80 | ((u >> 6) & 0x3f));
+		*d++ = (0x80 | (u & 0x3F));
 		len = 3;
 	} else if (u < 0x10FFFF) {
-		*d++ = (0xF0 | u >> 18);
-		*d++ = (0x80 | (u >> 12) & 0x3f);
-		*d++ = (0x80 | (u >> 6) & 0x3f);
-		*d++ = (0x80 | u & 0x3F);
+		*d++ = (0xF0 | (u >> 18));
+		*d++ = (0x80 | ((u >> 12) & 0x3f));
+		*d++ = (0x80 | ((u >> 6) & 0x3f));
+		*d++ = (0x80 | (u & 0x3F));
 		len = 4;
 	} else {
 		fprintf(stderr, "can't convert to U+%04x.\n", u);
