cd5dcd4d6ecae1a89e139ca4f20e9ccddcf38e48
LeavaTail <starbow.duster@gmail.com>
2020-09-14 23:14:25 +0900

    [Add] Replaces null lines to '*' when dump hexadecimal number

--- src/main.c
+++ src/main.c
@@ -222,10 +222,26 @@ int set_clusters(void *data, off_t index, size_t num)
  */
 void hexdump(FILE *out, void *data, size_t size)
 {
+	unsigned long skip = 0;
 	size_t line, byte = 0;
 	size_t count = size / 0x10;
+	const char zero[0x10] = {0};
 
 	for (line = 0; line < count; line++) {
+		if ((line != count - 1) && (!memcmp(data + line * 0x10, zero, 0x10))) {
+			switch (skip++) {
+				case 0:
+					break;
+				case 1:
+					pr_msg("*\n");
+					/* FALLTHROUGH */
+				default:
+					continue;
+			}
+		} else {
+			skip = 0;
+		}
+
 		pr_msg("%08lX:  ", line * 0x10);
 		for (byte = 0; byte < 0x10; byte++) {
 			pr_msg("%02X ", ((unsigned char *)data)[line * 0x10 + byte]);
