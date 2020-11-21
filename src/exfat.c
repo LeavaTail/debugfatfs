9fe851aebbe24d5b77e150dbf1f580a4a127595b
LeavaTail <starbow.duster@gmail.com>
2020-11-21 12:44:14 +0900

    [Fix] Occur overflow when new_cluster < cluster in exFAT

--- src/exfat.c
+++ src/exfat.c
@@ -2032,7 +2032,7 @@ int exfat_create(const char *name, uint32_t clu, int opt)
 	}
 
 	new_cluster_num = (i + count + 2) * sizeof(struct exfat_dentry) / info.cluster_size;
-	if (new_cluster_num - cluster_num) {
+	if (new_cluster_num > cluster_num) {
 		exfat_alloc_clusters(f, clu, new_cluster_num - cluster_num);
 		cluster_num = exfat_concat_cluster(f, clu, &data);
 		entries = (cluster_num * info.cluster_size) / sizeof(struct exfat_dentry);
