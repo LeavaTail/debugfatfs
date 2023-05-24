74b1e83e9a4409b4384701ab08cc33b1e0276afe
LeavaTail <starbow.duster@gmail.com>
2023-05-25 00:16:39 +0900

    [Improve] Add testcase for invalid usage
    Signed-off-by: LeavaTail <starbow.duster@gmail.com>

---
+++ tests/06_invalid_usage_check.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -u
+
+source tests/common.sh
+
+IMAGES=("fat12.img" "fat16.img" "fat32.img" "exfat.img")
+OUTPUT=data.dat
+
+function test_options () {
+	./debugfatfs -c 0 $1
+}
+
+function test_shell () {
+	expect -c "
+	set timeout 5
+	spawn ./debugfatfs -iq $1
+	expect \"/> \"
+	send \"create -a INVALID.TXT\n\"
+	expect \"/> \"
+	send \"create /00/DIR/FILE\n\"
+	expect \"/> \"
+	send \"remove /00/FILE1.TXT\n\"
+	expect \"/> \"
+	send \"remove /00/DIR/FILE\n\"
+	expect \"/> \"
+	send \"exit\n\"
+	expect eof
+	exit
+	"
+}
+
+function main() {
+	init_image
+
+	for fs in ${IMAGES[@]}; do
+		test_options ${fs}
+		test_shell ${fs}
+	done
+}
+
+### main function ###
+main "$@"
