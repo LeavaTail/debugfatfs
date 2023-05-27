ac1fbbb6b48d3bc4f048628e059545df25863e6a
LeavaTail <starbow.duster@gmail.com>
2023-05-27 11:42:24 +0900

    [Improve] Add testcase for UTF-16 to UTF-8 conversion
    Signed-off-by: LeavaTail <starbow.duster@gmail.com>

--- tests/unit_test/test.c
+++ tests/unit_test/test.c
@@ -65,6 +65,61 @@ void utf8_to_utf16_test_5(void)
 	return;
 }
 
+void utf16_to_utf8_test_1(void)
+{
+	uint16_t src[] = {0x41};
+	uint8_t dist = 0;
+
+	utf16s_to_utf8s(src, 1, &dist);
+	CU_ASSERT_EQUAL(dist, 'A');
+
+	return;
+}
+
+void utf16_to_utf8_test_2(void)
+{
+	uint16_t src[] = {0xBC};
+	uint8_t dist = 0;
+
+	utf16s_to_utf8s(src, 1, &dist);
+	CU_ASSERT_STRING_EQUAL(&dist, "¼");
+
+	return;
+}
+
+void utf16_to_utf8_test_3(void)
+{
+	uint16_t src[] = {0x3042};
+	uint8_t dist[3] = {0};
+
+	utf16s_to_utf8s(src, 1, dist);
+	CU_ASSERT_STRING_EQUAL(dist, "あ");
+
+	return;
+}
+
+void utf16_to_utf8_test_4(void)
+{
+	uint16_t src[] = {0x014C};
+	uint8_t dist[2] = {0};
+
+	utf16s_to_utf8s(src, 1, dist);
+	CU_ASSERT_STRING_EQUAL(dist, "Ō");
+
+	return;
+}
+
+void utf16_to_utf8_test_5(void)
+{
+	uint16_t src[] = {0xD842, 0xDFB7};
+	uint8_t dist[4] = {0};
+
+	utf16s_to_utf8s(src, 2, dist);
+	CU_ASSERT_STRING_EQUAL(dist, "𠮷");
+
+	return;
+}
+
 int main(void) {
 	int ret;
 	CU_pSuite suite;
@@ -77,6 +132,11 @@ int main(void) {
 	CU_add_test(suite, "NLS_Test_3", utf8_to_utf16_test_3);
 	CU_add_test(suite, "NLS_Test_4", utf8_to_utf16_test_4);
 	CU_add_test(suite, "NLS_Test_5", utf8_to_utf16_test_5);
+	CU_add_test(suite, "NLS_Test_6", utf16_to_utf8_test_1);
+	CU_add_test(suite, "NLS_Test_7", utf16_to_utf8_test_2);
+	CU_add_test(suite, "NLS_Test_8", utf16_to_utf8_test_3);
+	CU_add_test(suite, "NLS_Test_9", utf16_to_utf8_test_4);
+	CU_add_test(suite, "NLS_Test_10", utf16_to_utf8_test_5);
 
 	CU_basic_run_tests();
 	ret = CU_get_number_of_failures();
