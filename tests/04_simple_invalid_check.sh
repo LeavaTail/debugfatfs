3a627e70ffe70fba990428a094bab73b9041ae21
LeavaTail <starbow.duster@gmail.com>
2026-05-25 23:36:20 +0900

    [Improve] Add invalid testcase

--- tests/04_simple_invalid_check.sh
+++ tests/04_simple_invalid_check.sh
@@ -38,6 +38,12 @@ function test_shell () {
 	expect \"/> \"
 	send \"tail\n\"
 	expect \"/> \"
+	send \"dentry\n\"
+	expect \"/> \"
+	send \"dentry-set\n\"
+	expect \"/> \"
+	send \"dentry-raw\n\"
+	expect \"/> \"
 	send \"exit\n\"
 	expect eof
 	exit
@@ -73,7 +79,55 @@ function test_shell () {
 	expect \"/> \"
 	send \"tail A B\n\"
 	expect \"/> \"
+	send \"dentry A B\n\"
+	expect \"/> \"
+	send \"dentry-set A B C D\n\"
+	expect \"/> \"
+	send \"dentry-raw A B C D E F\n\"
+	expect \"/> \"
 	send \"nothing\n\"
+	expect \"/> \"
+	send \"exit\n\"
+	expect eof
+	exit
+	"
+	echo ""
+}
+
+function test_dentry_invalid () {
+	expect -c "
+	set timeout 5
+	spawn ./debugfatfs -iq $1
+	expect \"/> \"
+	send \"dentry-set /00/FILE1.TXT invalid 0x1\n\"
+	expect \"invalid dentry field: invalid\"
+	expect \"/> \"
+	send \"dentry-set /00/FILE1.TXT fat.short.INVALID 0x1\n\"
+	expect \"unsupported dentry field: fat.short.INVALID\"
+	expect \"/> \"
+	send \"dentry-set /00/FILE1.TXT fat.short.DIR_NTRes invalid\n\"
+	expect \"invalid value: invalid\"
+	expect \"/> \"
+	send \"dentry-set /00/NOFILE.TXT fat.short.DIR_NTRes 0x1\n\"
+	expect \"File is not found.\"
+	expect \"/> \"
+	send \"dentry-set /01/ABCDEFGHIJKLMNOPQRSTUVWXYZ! fat.lfn[99].LDIR_Chksum 0x1\n\"
+	expect \"invalid lfn index: 99\"
+	expect \"/> \"
+	send \"dentry-raw /00/FILE1.TXT short invalid 1 0x1\n\"
+	expect \"invalid raw dentry argument.\"
+	expect \"/> \"
+	send \"dentry-raw /00/FILE1.TXT short 0 3 0x1\n\"
+	expect \"invalid raw write size: 3\"
+	expect \"/> \"
+	send \"dentry-raw /00/FILE1.TXT short 0x20 1 0x1\n\"
+	expect \"raw write exceeds dentry size.\"
+	expect \"/> \"
+	send \"dentry-raw /00/NOFILE.TXT short 0 1 0x1\n\"
+	expect \"File is not found.\"
+	expect \"/> \"
+	send \"dentry-raw /00/FILE1.TXT invalid 0 1 0x1\n\"
+	expect \"invalid dentry selector: invalid\"
 	expect \"/> \"
 	send \"exit\n\"
 	expect eof
@@ -89,6 +143,7 @@ function main() {
 	for fs in ${IMAGES[@]}; do
 		test_options ${fs}
 		test_shell ${fs}
+		test_dentry_invalid ${fs}
 	done
 }
 
